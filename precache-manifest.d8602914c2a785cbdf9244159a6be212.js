f2c5c529dc27475edb8c530fd6b467fe53d77e48
unknown <arefin.ashiful@gmail.com>
2020-05-26 13:50:32 +0600

    Updates

---
+++ precache-manifest.d8602914c2a785cbdf9244159a6be212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac38078aeba724f86a089b3c5b60e014",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "f0fe8237207b1f4f680d",
+    "url": "/Monsters-Rolodex/static/css/main.133bd34b.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/Monsters-Rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0fe8237207b1f4f680d",
+    "url": "/Monsters-Rolodex/static/js/main.658c7295.chunk.js"
+  },
+  {
+    "revision": "bddd2872dd5650c52730",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.dc822ace.js"
+  }
+]);
